d55959f2c9c69b2e0974f389b8d632698b9595b2
davcamalv <dcamalv@gmail.com>
2019-04-30 02:16:38 +0200

    Version 1.0

---
+++ Acme-Rookie/src/main/java/domain/Provider.java
@@ -0,0 +1,27 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Provider extends Actor {
+
+	private String	make;
+
+
+	@NotBlank
+	@SafeHtml
+	public String getMake() {
+		return this.make;
+	}
+
+	public void setMake(final String make) {
+		this.make = make;
+	}
+}
