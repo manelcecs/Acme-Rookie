a9ca9a1bb602fa28dcf5a4c182995f37d8f697c6
davcamalv <dcamalv@gmail.com>
2019-05-03 01:45:04 +0200

    Version 1.0

--- Acme-Rookie/src/main/java/repositories/ProviderRepository.java
+++ Acme-Rookie/src/main/java/repositories/ProviderRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,7 @@ public interface ProviderRepository extends JpaRepository<Provider, Integer> {
 	@Query("select p from Provider p where p.userAccount.id = ?1")
 	Provider findByPrincipal(int principalId);
 
+	//ACME-ROOKIE-----------------------------------------------------------------
+	@Query("select p from Sponsorship s join s.provider p where (select count(s) from Sponsorship s join s.provider pr where pr.id = p.id)>=(select avg(1*(select count(s) from Sponsorship s where s.provider.id = pro.id)) from Provider pro)")
+	Collection<Provider> getProviders10RateOfAvgOfSponsorships();
 }
--- Acme-Rookie/src/main/java/services/ProviderService.java
+++ Acme-Rookie/src/main/java/services/ProviderService.java
@@ -1,12 +1,17 @@
 
 package services;
 
+import java.util.Collection;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import repositories.ProviderRepository;
+import security.UserAccount;
+import utiles.AuthorityMethods;
 import domain.Provider;
 
 @Service
@@ -14,10 +19,21 @@ import domain.Provider;
 public class ProviderService {
 
 	@Autowired
-	ProviderRepository	providerRepository;
+	private ProviderRepository	providerRepository;
 
 
-	public Provider findByPrincipal(final int idPrincipal) {
-		return this.providerRepository.findByPrincipal(idPrincipal);
+	public Provider findByPrincipal(final UserAccount principal) {
+		return this.providerRepository.findByPrincipal(principal.getId());
 	}
+
+	public Collection<Provider> findAll() {
+		return this.providerRepository.findAll();
+	}
+
+	public Collection<Provider> getProviders10RateOfAvgOfSponsorships() {
+		Assert.isTrue(AuthorityMethods.chechAuthorityLogged("ADMINISTRATOR"));
+		return this.providerRepository.getProviders10RateOfAvgOfSponsorships();
+
+	}
+
 }
