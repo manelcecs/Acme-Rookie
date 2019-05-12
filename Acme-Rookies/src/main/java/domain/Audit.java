a3ca30e5b58a5eba629cdfda0d9243979806576e
manuelc.p.g@gmail.com <manuelc.p.g@gmail.com>
2019-05-12 11:04:29 +0100

    Rename
    Proyecto renombrado, con el nuevo banner
    -Añadido los indices

---
+++ Acme-Rookies/src/main/java/domain/Audit.java
@@ -0,0 +1,86 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "draft")
+})
+public class Audit extends DomainEntity {
+
+	private boolean		draft;
+	private Integer		score;
+	private Date		moment;
+	private String		text;
+
+	private Position	position;
+
+
+	public boolean getDraft() {
+		return this.draft;
+	}
+
+	public void setDraft(final boolean draft) {
+		this.draft = draft;
+	}
+
+	@Range(min = 0, max = 10)
+	@NotNull
+	public Integer getScore() {
+		return this.score;
+	}
+
+	public void setScore(final Integer score) {
+		this.score = score;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss")
+	@NotNull
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	@NotBlank
+	@SafeHtml
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(final String text) {
+		this.text = text;
+	}
+
+	@Valid
+	@ManyToOne(optional = false)
+	public Position getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(final Position position) {
+		this.position = position;
+	}
+
+}
---
+++ Acme-Rookies/src/main/java/domain/Item.java
@@ -0,0 +1,82 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.SafeHtml;
+
+@Entity
+@Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "name")
+})
+public class Item extends DomainEntity {
+
+	private String				name;
+	private String				description;
+	private Collection<String>	links;
+	private Collection<String>	pictures;
+
+	private Provider			provider;
+
+
+	@NotBlank
+	@SafeHtml
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@NotBlank
+	@SafeHtml
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(final String description) {
+		this.description = description;
+	}
+
+	@ElementCollection
+	@NotEmpty
+	public Collection<String> getLinks() {
+		return this.links;
+	}
+
+	public void setLinks(final Collection<String> links) {
+		this.links = links;
+	}
+
+	@ElementCollection
+	public Collection<String> getPictures() {
+		return this.pictures;
+	}
+
+	public void setPictures(final Collection<String> pictures) {
+		this.pictures = pictures;
+	}
+
+	@Valid
+	@ManyToOne(optional = false)
+	public Provider getProvider() {
+		return this.provider;
+	}
+
+	public void setProvider(final Provider provider) {
+		this.provider = provider;
+	}
+}
