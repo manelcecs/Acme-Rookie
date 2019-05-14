f0ee68787275cbe081beb55250a1b5f1bc7eee5d
manuelc.p.g@gmail.com <manuelc.p.g@gmail.com>
2019-05-14 11:26:58 +0100

    Minor Fixes

--- Acme-Rookies/src/main/java/domain/AdminConfig.java
+++ Acme-Rookies/src/main/java/domain/AdminConfig.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.SafeHtml;
+import org.hibernate.validator.constraints.URL;
 
 import forms.AdminConfigForm;
 
@@ -105,7 +106,7 @@ public class AdminConfig extends DomainEntity {
 	}
 
 	@NotBlank
-	//@URL
+	@URL
 	@SafeHtml
 	public String getBannerURL() {
 		return this.bannerURL;
